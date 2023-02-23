09dfac2943c74867c449b318a238b86269069e17
jiokeokwuosa <okwuosachijioke@gmail.com>
2023-02-23 18:47:42 +0100

    updated rebase logic to reduce gas consumption...

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./DevToken.sol";
-import "./library/PriceConverter.sol";
+import "./library/PriceFeed.sol";
 import "hardhat/console.sol";
 
 error TokenFactory__InsufficientFund();
@@ -19,7 +19,7 @@ error TokenFactory__InsufficientFund();
  * The contract will implement periodic rebalancing
  */
 contract TokenFactory is ReentrancyGuard{
-    using PriceConverter for AggregatorV3Interface;
+    using PriceFeed for AggregatorV3Interface;
     using Math for uint256;
 
     // State variables
@@ -27,6 +27,7 @@ contract TokenFactory is ReentrancyGuard{
     address private immutable i_baseTokenAddress;
     address[] private s_funders;
     AggregatorV3Interface private immutable i_priceFeed;
+    mapping (address => bool) inserted;
 
     // Events
     event AssetBought(address indexed recipient, uint256 amount);
@@ -76,7 +77,10 @@ contract TokenFactory is ReentrancyGuard{
     function buyAsset() public payable nonReentrant {
         sendToken(0, msg.sender, msg.value);
         sendToken(1, msg.sender, msg.value);
-        s_funders.push(msg.sender);
+        if(!inserted[msg.sender]){
+            inserted[msg.sender] = true;
+            s_funders.push(msg.sender);
+        }  
         emit AssetBought(msg.sender, msg.value);
     }
 
@@ -89,30 +93,26 @@ contract TokenFactory is ReentrancyGuard{
         emit AssetWithdrawn(msg.sender, _amount);
     }
 
-    function getBaseTokenAddress() public view returns (address) {
-        return i_baseTokenAddress;
-    }
-
-    function getFunderAddressByIndex(uint index) public view returns (address) {
-        return s_funders[index];
-    }
-
-    function rebase() public {        
-        address[] memory funders = s_funders;      
-        uint256 rebasePrice = i_priceFeed.getPrice()/1e18;
+    function rebase() public  {        
+        address[] memory funders = s_funders; 
+        uint256 rebasePrice = i_priceFeed.getPrice()/1e18;       
         uint256 asset1Price = rebasePrice.ceilDiv(3); // this should be gotten from the oracle
-        uint256 asset2Price = rebasePrice - asset1Price;
+        uint256 asset2Price = rebasePrice - asset1Price;    
         uint256 asset1Balance;
         uint256 asset2Balance;
         uint256 accountValue;
         uint256 rollOverValueInUSD;
-        uint256 rollOverDivisor;        
+        uint256 rollOverDivisor; 
+        uint256 length = funders.length;    
+        uint256 rollOverValue;   
+        uint256 asset1ValueEth;
+        uint256 asset2ValueEth;
        
         for (
             uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
+            funderIndex < length; 
+            funderIndex++                     
+        ) {  
             address funder = funders[funderIndex];
             asset1Balance = getBalance(0, funder) / 1e18;
             asset2Balance = getBalance(1, funder) / 1e18;            
@@ -121,11 +121,30 @@ contract TokenFactory is ReentrancyGuard{
                 (asset2Balance * asset2Price);
             rollOverValueInUSD = accountValue / 2;
             rollOverDivisor = rebasePrice/2; 
-           
-            burnToken(0, funder, getBalance(0, funder));
-            burnToken(1, funder, getBalance(1, funder));            
-            sendToken(0, funder, (rollOverValueInUSD *1e18/rollOverDivisor* 1e18)/1e18);
-            sendToken(1, funder, (rollOverValueInUSD *1e18/rollOverDivisor* 1e18)/1e18);
+            rollOverValue = (rollOverValueInUSD *1e18/rollOverDivisor* 1e18)/1e18;
+            asset1ValueEth = asset1Balance*1e18;
+            asset2ValueEth = asset2Balance*1e18;        
+
+            if(rollOverValue > asset1ValueEth){
+                sendToken(0, funder, (rollOverValue - asset1ValueEth));
+            }else{
+                burnToken(0, funder, (asset1ValueEth - rollOverValue));
+            }
+            
+            if(rollOverValue > asset2ValueEth){
+                sendToken(1, funder, (rollOverValue - asset2ValueEth));
+            }else{
+                burnToken(1, funder, (asset2ValueEth - rollOverValue));
+            }
+
         }
+    }
+    
+    function getBaseTokenAddress() public view returns (address) {
+        return i_baseTokenAddress;
+    }
+
+    function getFunderAddressByIndex(uint index) public view returns (address) {
+        return s_funders[index];
     }
 }
