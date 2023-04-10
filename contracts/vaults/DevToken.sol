eaad40b6000d1967231fa18b9a65b9146a543253
jiokeokwuosa <okwuosachijioke@gmail.com>
2023-04-10 19:18:57 +0100

    Implemented ERC777

--- contracts/vaults/DevToken.sol
+++ contracts/vaults/DevToken.sol
@@ -2,14 +2,12 @@
 
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 import "./TokenFactory.sol";
 
 error DevToken__NotTokenFactory();
 
-contract DevToken is ERC20, Ownable, ERC20Permit {
+contract DevToken is ERC777 {
     TokenFactory private immutable tokenFactory;
 
     modifier onlyTokenFactory() {
@@ -21,40 +19,55 @@ contract DevToken is ERC20, Ownable, ERC20Permit {
     constructor(
         string memory tokenName,
         string memory tokenSymbol,
-        address factoryAddress
-    ) ERC20(tokenName, tokenSymbol) ERC20Permit(tokenName) {
+        address factoryAddress,
+        address[] memory defaultOperators
+    ) ERC777(tokenName, tokenSymbol, defaultOperators) {
         tokenFactory = TokenFactory(factoryAddress);
     }
 
     function mint(address receiver, uint256 amount) public onlyTokenFactory {
-        _mint(receiver, amount);
+        _mint(receiver, amount, "", "");
     }
 
     function burn(address account, uint256 amount) public onlyTokenFactory {
-        _burn(account, amount);
+        _burn(account, amount, "", "");
     }
 
+    /** @dev See {IERC777-transfer}. */
     function transfer(
         address to,
         uint256 amount
     ) public override returns (bool) {
         address owner_ = msg.sender;
-        if (
-            tokenFactory.getUserLastRebaseCount(owner_) !=
-            tokenFactory.getScallingFactorLength()
-        ) {
+        if (hasPendingRebase(owner_)) {
             tokenFactory.applyRebase(owner_);
         }
         tokenFactory.updateUserLastRebaseCount(to);
-        _transfer(owner_, to, amount);
+        super.transfer(to, amount);
         return true;
     }
 
+    /**
+     * @dev See {IERC777-send}.
+     *
+     * Also emits a {IERC20-Transfer} event for ERC20 compatibility.
+     */
+    function send(
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) public override {
+        address owner_ = msg.sender;
+        if (hasPendingRebase(owner_)) {
+            tokenFactory.applyRebase(owner_);
+        }
+        tokenFactory.updateUserLastRebaseCount(recipient);
+        _send(_msgSender(), recipient, amount, data, "", true);
+    }
+    
+    /** @dev See {IERC777-balanceOf}. */
     function balanceOf(address account) public view override returns (uint256) {
-        if (
-            tokenFactory.getUserLastRebaseCount(account) !=
-            tokenFactory.getScallingFactorLength()
-        ) {
+        if (hasPendingRebase(account)) {
             return tokenFactory.calculateRollOverValue(account);
         } else {
             return unScaledbalanceOf(account);
@@ -63,5 +76,11 @@ contract DevToken is ERC20, Ownable, ERC20Permit {
 
     function unScaledbalanceOf(address account) public view returns (uint256) {
         return super.balanceOf(account);
+    }
+
+    function hasPendingRebase(address account) public view returns (bool) {
+        return
+            tokenFactory.getUserLastRebaseCount(account) !=
+            tokenFactory.getScallingFactorLength();
     }
 }
