19c0db67599c5cd197473d5e0adc62d13782055a
Haidar Ally <haidarally@hotmail.com>
2023-10-02 16:51:15 +0400

    added comments for SmartTokens

--- contracts/interfaces/IERC20Update.sol
+++ contracts/interfaces/IERC20Update.sol
@@ -4,7 +4,17 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title ERC20 Token with Permit Extension Interface
+/// @dev This interface extends the standard ERC20 interface with a permit function
 interface IERC20Update is IERC20 {
+    /// @notice Allows for gasless approvals by using off-chain generated signatures.
+    /// @param owner The owner of the tokens.
+    /// @param spender The address which will spend the tokens.
+    /// @param value The amount of tokens to be spent.
+    /// @param deadline The time after which the permit signature is invalid.
+    /// @param v The recovery id of the ECDSA signature.
+    /// @param r Output value r of the ECDSA signature.
+    /// @param s Output value s of the ECDSA signature.
     function permit(
         address owner,
         address spender,
--- contracts/vaults/BaseContract.sol
+++ contracts/vaults/BaseContract.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-// Link to Sanction List https://go.chainalysis.com/chainalysis-oracle-docs.html
+/// @title SanctionsList Interface
+/// @dev This interface defines a method for checking if an address is on a sanctions list.
+/// Implementing contracts should return true if the address is sanctioned, false otherwise.
 interface SanctionsList {
     function isSanctioned(address addr) external view returns (bool);
 }
@@ -15,7 +17,13 @@ error BaseContract__WithdrawCircuitBreaker();
 error BaseContract__TransferCircuitBreaker();
 error BaseContract__RebaseCircuitBreaker();
 
-
+// @title BaseContract
+/// @dev This contract primarily tracks sanctioned wallets from Chainalysis and
+/// provides circuit breaker functionalities. It is utilized by TokenFactory and SmartTokens
+/// to manage interactions with potentially sanctioned addresses and to handle
+/// emergency situations by halting certain operations.
+/// @notice Please refer to Chainalysis documentation (https://go.chainalysis.com/chainalysis-oracle-docs.html)
+/// for the sanctions list.
 contract BaseContract is Initializable, OwnableUpgradeable {
     address private sanctionsContract;
 
@@ -24,6 +32,8 @@ contract BaseContract is Initializable, OwnableUpgradeable {
     bool private transferCircuitBreaker;
     bool private rebaseCircuitBreaker;
 
+    /// @dev Checks if the address is sanctioned.
+    /// @param addressToCheck The address to be checked.
     modifier onlyNotSanctioned(address addressToCheck) {
         SanctionsList sanctionsList = SanctionsList(sanctionsContract);
         bool isSanctionedAddress = sanctionsList.isSanctioned(addressToCheck);
@@ -31,28 +41,36 @@ contract BaseContract is Initializable, OwnableUpgradeable {
         _;
     }
 
-
     // circuit breaker modifiers
+    /// @dev Reverts if the deposit circuit breaker is active.
+    /// This modifier is used to halt user deposits in emergency situations.
     modifier stopDeposit() {
         if (depositCircuitBreaker) revert BaseContract__DepositCircuitBreaker();
         _;
     }
-
+    /// @dev Reverts if the withdraw circuit breaker is active.
+    /// This modifier is used to halt user withdrawals in emergency situations.
     modifier stopWithdraw() {
-        if (withdrawCircuitBreaker) revert BaseContract__WithdrawCircuitBreaker();
+        if (withdrawCircuitBreaker)
+            revert BaseContract__WithdrawCircuitBreaker();
         _;
     }
-
+    /// @dev Reverts if the transfer circuit breaker is active.
+    /// This modifier is used to halt smart token transfers in emergency situations.
     modifier stopTransfer() {
-        if (transferCircuitBreaker) revert BaseContract__TransferCircuitBreaker();
+        if (transferCircuitBreaker)
+            revert BaseContract__TransferCircuitBreaker();
         _;
     }
-
+    /// @dev Reverts if the rebase circuit breaker is active.
+    /// This modifier is used to halt rebase operations in emergency situations.
     modifier stopRebase() {
         if (rebaseCircuitBreaker) revert BaseContract__RebaseCircuitBreaker();
         _;
     }
 
+    /// @dev Initializes the BaseContract with a sanctions contract address.
+    /// @param sanctionsContract_ The address of the sanctions contract.
     function __BaseContract_init(
         address sanctionsContract_
     ) internal onlyInitializing {
@@ -65,22 +83,37 @@ contract BaseContract is Initializable, OwnableUpgradeable {
     }
 
     // circuit breaker functions
+    /// @notice Toggles the deposit circuit breaker on or off.
+    /// @dev This function allows the contract owner to halt or resume deposit operations in case of emergency.
+    /// Only the owner can call this function.
     function toggleDepositCircuitBreaker() external onlyOwner {
         depositCircuitBreaker = !depositCircuitBreaker;
     }
 
+    /// @notice Toggles the withdraw circuit breaker on or off.
+    /// @dev This function allows the contract owner to halt or resume withdraw operations in case of emergency.
+    /// Only the owner can call this function.
     function toggleWithdrawCircuitBreaker() external onlyOwner {
         withdrawCircuitBreaker = !withdrawCircuitBreaker;
     }
 
+    /// @notice Toggles the transfer circuit breaker on or off.
+    /// @dev This function allows the contract owner to halt or resume smart token transfers in case of emergency.
+    /// Only the owner can call this function.
     function toggleTransferCircuitBreaker() external onlyOwner {
         transferCircuitBreaker = !transferCircuitBreaker;
     }
 
+    /// @notice Toggles the rebase circuit breaker on or off.
+    /// @dev This function allows the contract owner to halt or resume rebase operations in case of emergency.
+    /// Only the owner can call this function.
     function toggleRebaseCircuitBreaker() external onlyOwner {
         rebaseCircuitBreaker = !rebaseCircuitBreaker;
     }
 
+    /// @notice Activates all circuit breakers, halting deposit, withdraw, transfer, and rebase operations.
+    /// @dev This function allows the contract owner to halt critical operations in case of emergency.
+    /// Only the owner can call this function.
     function stopAllCircuitBreakers() external onlyOwner {
         depositCircuitBreaker = true;
         withdrawCircuitBreaker = true;
@@ -88,6 +121,9 @@ contract BaseContract is Initializable, OwnableUpgradeable {
         rebaseCircuitBreaker = true;
     }
 
+    /// @notice Deactivates all circuit breakers, resuming deposit, withdraw, transfer, and rebase operations.
+    /// @dev This function allows the contract owner to resume critical operations after an emergency halt.
+    /// Only the owner can call this function.
     function resumeAllCircuitBreakers() external onlyOwner {
         depositCircuitBreaker = false;
         withdrawCircuitBreaker = false;
@@ -95,18 +131,26 @@ contract BaseContract is Initializable, OwnableUpgradeable {
         rebaseCircuitBreaker = false;
     }
 
+    /// @notice Checks if the deposit circuit breaker is active.
+    /// @return A boolean value indicating whether the deposit circuit breaker is active pr not
     function isDepositCircuitBreaker() external view returns (bool) {
         return depositCircuitBreaker;
     }
 
+    /// @notice Checks if the withdraw circuit breaker is active.
+    /// @return A boolean value indicating whether the withdraw circuit breaker is active or not.
     function isWithdrawCircuitBreaker() external view returns (bool) {
         return withdrawCircuitBreaker;
     }
 
+    /// @notice Checks if the transfer circuit breaker is active.
+    /// @return A boolean value indicating whether the transfer circuit breaker is active or not.
     function isTransferCircuitBreaker() external view returns (bool) {
         return transferCircuitBreaker;
     }
 
+    /// @notice Checks if the rebase circuit breaker is active.
+    /// @return A boolean value indicating whether the rebase circuit breaker is active or not.
     function isRebaseCircuitBreaker() external view returns (bool) {
         return rebaseCircuitBreaker;
     }
