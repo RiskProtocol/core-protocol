84ec04e4f4e3ee9c19c10971fa99fd7ef6d1009a
Haidar Ally <haidarally@hotmail.com>
2024-03-08 17:46:32 +0400

    updated config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 550,
           },
           viaIR: false,
         },
