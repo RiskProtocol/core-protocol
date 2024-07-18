2fcb334fae24b535e948a9ce0928e27c902e6791
haidarally <haidarally@hotmail.com>
2024-07-18 17:04:27 +0400

    updated hardhat runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 2000,
           },
           viaIR: true,
         },
