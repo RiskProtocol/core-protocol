7d0ba17eda6511093d2659eb040d4408279ef03a
haidarally <haidarally@hotmail.com>
2024-09-16 18:21:30 +0400

    added method to manually set the last rebase time

--- contracts/vaults/TokenFactory.sol
+++ contracts/vaults/TokenFactory.sol
@@ -1240,6 +1240,11 @@ contract TokenFactory is
         emit PremiumDrained(receiver, amount);
     }
 
+    //function to update the last timestamp
+    function updateLastRebalanceTimeStamp(uint256 newTimeStamp) external onlyOwner {
+        lastTimeStamp = newTimeStamp;
+    }
+
     /// @notice Retrieves the `scheduledRebalance` struct at the given sequence number.
     /// @dev This function is a getter for a single `scheduledRebalance` struct.
     /// @param sequenceNumber The sequence number of the `scheduledRebalances` mapping to retrieve.
